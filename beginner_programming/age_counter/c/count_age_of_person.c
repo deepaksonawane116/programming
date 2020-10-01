eea0ce154714d975800c864562580cd4b52e418f
deepaksonawane116 <72155548+deepaksonawane116@users.noreply.github.com>
2020-10-01 09:11:23 +0530

    Update count_age_of_person.c

--- beginner_programming/age_counter/c/count_age_of_person.c
+++ beginner_programming/age_counter/c/count_age_of_person.c
@@ -2,9 +2,9 @@
 
 int main()
 {
-	int BirthMonth, BirthYear;
-	int CurrentMonth, CurrentYear;
-	int AgeYear, AgeMonth;
+	int BirthMonth, BirthYear,BirthDate;
+	int CurrentMonth, CurrentYear,CurrentDate;
+	int AgeYear, AgeMonth,AgeDate;
 	
 	printf("\n\n\t\t\tCount the age person\n\n");
 	printf("Enter Your Birth Year(Eg:1989):");
@@ -13,16 +13,23 @@ int main()
 	printf("\n\nEnter Your Birth Month(Eg:7):");
 	scanf("%d",&BirthMonth);
 	
+	printf("\n\nEnter Your Birth Date(Eg:5):");
+	scanf("%d",&BirthDate);
+	
 	printf("\nEnter The Current Month(Eg:7):");
 	scanf("%d",&CurrentMonth);
 	
 	printf("\nEnter The Current Year(Eg:2010):");
 	scanf("%d",&CurrentYear);
 	
+	printf("\nEnter The Current Date(Eg:20):");
+	scanf("%d",&CurrentDate);
+	
 	AgeYear=CurrentYear-BirthYear;
 	AgeMonth=CurrentMonth-BirthMonth;
+	AgeDate=CurrentDate-BirthDate;
 	
-	printf("\n\n\t\tYour Age is %d Years And %d Months ",AgeYear,AgeMonth);
+	printf("\n\n\t\tYour Age is %d Years And %d Months And %d Days ",AgeYear,AgeMonth,AgeDate);
 	
 	return 0;
 }
